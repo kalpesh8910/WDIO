de14ec33bb0303c3236ae893646b38e76084d504
kalpesh8910 <prajapatikalpesh8910@gmail.com>
2022-06-26 09:39:41 +0530

    intitial commit

---
+++ test/features/demo/demo.feature
@@ -0,0 +1,13 @@
+Feature: demo feature
+
+    @demo
+
+    Scenario Outline: search from google
+        Given Google Page is opened
+        When Search with <SearchIteam>
+        Then Click on frist result search
+        Then Url should match <ExpectedUrl>
+
+        Examples:
+            | TestId       | SearchIteam | ExpectedUrl           |
+            | DEMO_TEST_01 | WDIO        | https://webdriver.io/ |
---
+++ test/features/step-definitions/demo.ts
@@ -0,0 +1,29 @@
+import { Given, When, Then } from "@wdio/cucumber-framework";
+import chai from "chai"
+
+Given(/^Google Page is opened$/, async function () {
+  console.log(`Before opening browser`);
+  await browser.url("https://www.google.com/");
+  await browser.maximizeWindow();
+  await browser.pause(10000);
+  console.log(`After opening browser`);
+});
+
+When(/^Search with (.*)$/, async function (SearchIteam) {
+  let ele = await $(`[name=q]`);
+  ele.setValue(SearchIteam);
+  await browser.pause(10000);
+  await browser.keys("Enter");
+});
+
+Then(/^Click on frist result search$/, async function () {
+  let ele = await $(`<h3>`);
+  ele.click();
+  await browser.pause(10000);
+});
+
+Then(/^Url should match (.*)$/, async function (ExpectedUrl) {
+  console.log(` >> ExpectedUrl: $(ExpectedUrl)`);
+  await browser.pause(10000);
+
+});
