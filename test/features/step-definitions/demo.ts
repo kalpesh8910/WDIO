0faa18362d1ad4e0a6b5b04237f2595b4c2fbe71
kalpesh8910 <prajapatikalpesh8910@gmail.com>
2022-06-27 17:54:02 +0530

    27 commit

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -22,11 +22,41 @@ Then(/^Click on frist result search$/, async function () {
   await browser.pause(5000);
 });
 
-
-
-
 Then(/^Url should match (.*)$/, async function (ExpectedUrl) {
   console.log(` >> ExpectedUrl: $(ExpectedUrl)`);
   await browser.pause(5000);
 
 });
+
+/*
+* Web Interactions
+*/
+
+Given(/^A web page is opened$/, async function(){
+
+  await browser.url("/inputs")
+  await browser.setTimeout({implicit: 15000, pageLoad: 10000})
+  await browser.maximizeWindow()
+  await browser.pause(5000)
+
+})
+
+When(/^Perform web interaction$/, async function() {
+  
+  //1. Input box
+
+  //Actions:
+  // 1. Type into input Box
+  // 2. Clear the field and type or just addvalue
+  // 3. Click and type
+  // 4. Slow typing
+
+  
+  let ele1 = await (await $('[type=number]')).setValue("12345")
+  await browser.pause(5000)
+//  await ele1.setValue('12345')
+  browser.pause(3000)
+
+  //await browser.debug()
+
+})
