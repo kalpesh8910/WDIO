bddadf3753b93b4f3abf0816c48731f996aa7083
kalpesh8910 <prajapatikalpesh8910@gmail.com>
2022-06-27 11:19:01 +0530

    today comit

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -5,25 +5,28 @@ Given(/^Google Page is opened$/, async function () {
   console.log(`Before opening browser`);
   await browser.url("https://www.google.com/");
   await browser.maximizeWindow();
-  await browser.pause(10000);
+  await browser.pause(5000);
   console.log(`After opening browser`);
 });
 
 When(/^Search with (.*)$/, async function (SearchIteam) {
   let ele = await $(`[name=q]`);
   ele.setValue(SearchIteam);
-  await browser.pause(10000);
+  await browser.pause(5000);
   await browser.keys("Enter");
 });
 
 Then(/^Click on frist result search$/, async function () {
   let ele = await $(`<h3>`);
   ele.click();
-  await browser.pause(10000);
+  await browser.pause(5000);
 });
+
+
+
 
 Then(/^Url should match (.*)$/, async function (ExpectedUrl) {
   console.log(` >> ExpectedUrl: $(ExpectedUrl)`);
-  await browser.pause(10000);
+  await browser.pause(5000);
 
 });
